64ce06e8ad234136f43497f81481d21fadb803a2
hanjinglong <hanjinglong@saicmotor.com>
2022-05-07 21:04:56 +0800

    libsel4vmmplatsupport: amend virtq's TX queue_size
    amend virtq's TX queue_size with TX_QUEUE
    
    Signed-off-by: hanjinglong <hjlallen@qq.com>

--- libsel4vmmplatsupport/src/drivers/virtio_emul.c
+++ libsel4vmmplatsupport/src/drivers/virtio_emul.c
@@ -132,7 +132,7 @@ virtio_emul_t *virtio_emul_init(ps_io_ops_t io_ops, int queue_size, vm_t *vm, vo
     emul->virtq.queue_size[TX_QUEUE] = queue_size;
     /* create dummy rings. we never actually dereference the rings so they can be null */
     vring_init(&emul->virtq.vring[RX_QUEUE], emul->virtq.queue_size[RX_QUEUE], 0, VIRTIO_PCI_VRING_ALIGN);
-    vring_init(&emul->virtq.vring[TX_QUEUE], emul->virtq.queue_size[RX_QUEUE], 0, VIRTIO_PCI_VRING_ALIGN);
+    vring_init(&emul->virtq.vring[TX_QUEUE], emul->virtq.queue_size[TX_QUEUE], 0, VIRTIO_PCI_VRING_ALIGN);
     emul->io_in = emul_io_in;
     emul->io_out = emul_io_out;
 
